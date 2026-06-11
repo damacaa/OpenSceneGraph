2190dbd9b7ffc02ad3ec315e90b87352926a98d9
Daniel Martí <49535803+damacaa@users.noreply.github.com>
2026-06-11 11:47:32 +0200

    Fix missing serializer

---
+++ test_empty.cpp
@@ -0,0 +1,18 @@
+#include <osg/Group>
+#include <osg/MatrixTransform>
+#include <osgDB/ReadFile>
+#include <osgDB/WriteFile>
+
+int main() {
+    osg::ref_ptr<osg::Group> root = new osg::Group;
+    osg::ref_ptr<osg::MatrixTransform> mt1 = new osg::MatrixTransform;
+    mt1->setName(\
+EmptyTransform1\);
+    osg::ref_ptr<osg::MatrixTransform> mt2 = new osg::MatrixTransform;
+    mt2->setName(\EmptyTransform2\);
+    root->addChild(mt1.get());
+    root->addChild(mt2.get());
+    osgDB::writeNodeFile(*root, \test_empty.osgb\);
+    return 0;
+}
+EOF
